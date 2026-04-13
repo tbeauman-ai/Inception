53f20f79fd559fa1a832a0e6adf7b1cdf6adefb6
Beaumanoir <theo.beaumanoir@gmail.com>
2026-04-13 13:15:56 +0200

    php qui clear l'env a chaque fois et du coup mon env sert a rien : fixed dans le Dockerfile du Wordpress

--- srcs/requirements/mariadb/conf/init.sh
+++ srcs/requirements/mariadb/conf/init.sh
@@ -8,14 +8,20 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
     mysql_install_db --user=mysql --datadir=/var/lib/mysql
 fi
 
-exec mysqld --user=mysql --port=3306 --bind-address=0.0.0.0 --skip-networking=0
+mysqld --user=mysql & MYSQL_PID=$!
 
-mysqld --user=mysql --bootstrap << EOF
-FLUSH PRIVILEGES;
-CREATE DATABASE IF NOT EXISTS ${DB_NAME__};
-CREATE USER IF NOT EXISTS '${DB_USER__}'@'%' IDENTIFIED BY '${DB_PASSWORD__}';
-GRANT ALL PRIVILEGES ON ${DB_NAME__}.* TO '${DB_USER__}'@'%';
+until mysqladmin ping --silent; do
+    sleep 1
+done
+
+mysql -u root << EOF
+CREATE DATABASE IF NOT EXISTS ${DB_NAME};
+CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
+GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
 
-exec mysqld --user=mysql
+mysqladmin shutdown
+wait $MYSQL_PID
+
+exec mysqld --user=mysql 
--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -1,20 +1,20 @@
 <?php
 
-define('DB_NAME', getenv('DB_NAME__'));
-define('DB_USER', getenv('DB_USER__'));
-define('DB_PASSWORD',  getenv('DB_PASSWORD__'));
-define('DB_HOST', getenv('DB_HOST__'));
+define('DB_NAME', getenv('DB_NAME'));
+define('DB_USER', getenv('DB_USER'));
+define('DB_PASSWORD',  getenv('DB_PASSWORD'));
+define('DB_HOST', 'mariadb:3306');
 define('DB_CHARSET', 'utf8');
 
 
-define('AUTH_KEY', getenv('AUTH_KEY__'));
-define('SECURE_AUTH_KEY', getenv('SECURE_AUTH_KEY__'));
-define('LOGGED_IN_KEY', getenv('LOGGED_IN_KEY__'));
-define('NONCE_KEY', getenv('NONCE_KEY__'));
-define('AUTH_SALT', getenv('AUTH_SALT__'));
-define('SECURE_AUTH_SALT', getenv('SECURE_AUTH_SALT__'));
-define('LOGGED_IN_SALT', getenv('LOGGED_IN_SALT__'));
-define('NONCE_SALT', getenv('NONCE_SALT__'));
+define('AUTH_KEY', getenv('AUTH_KEY'));
+define('SECURE_AUTH_KEY', getenv('SECURE_AUTH_KEY'));
+define('LOGGED_IN_KEY', getenv('LOGGED_IN_KEY'));
+define('NONCE_KEY', getenv('NONCE_KEY'));
+define('AUTH_SALT', getenv('AUTH_SALT'));
+define('SECURE_AUTH_SALT', getenv('SECURE_AUTH_SALT'));
+define('LOGGED_IN_SALT', getenv('LOGGED_IN_SALT'));
+define('NONCE_SALT', getenv('NONCE_SALT'));
 
 $table_prefix = 'wp_';
 define('WP_DEBUG', true);
